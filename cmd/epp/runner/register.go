93625157dba55c4d46d8db6711be3ddf8afff596
David Whyte-Gray <40244437+dagrayvid@users.noreply.github.com>
2025-08-26 16:53:36 -0400

    Add Jooho weighted random on top of v0.5.1

--- cmd/epp/runner/register.go
+++ cmd/epp/runner/register.go
@@ -38,6 +38,7 @@ func RegisterAllPlugins() {
 	plugins.Register(prefix.PrefixCachePluginType, prefix.PrefixCachePluginFactory)
 	plugins.Register(picker.MaxScorePickerType, picker.MaxScorePickerFactory)
 	plugins.Register(picker.RandomPickerType, picker.RandomPickerFactory)
+	plugins.Register(picker.WeightedRandomPickerType, picker.WeightedRandomPickerFactory)
 	plugins.Register(profile.SingleProfileHandlerType, profile.SingleProfileHandlerFactory)
 	plugins.Register(scorer.KvCacheScorerType, scorer.KvCacheScorerFactory)
 	plugins.Register(scorer.QueueScorerType, scorer.QueueScorerFactory)
